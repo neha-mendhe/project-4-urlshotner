4b1c998fa728d12cf77452252990f7f1fde141fe
Amit <amit261199@gmail.com>
2022-05-17 15:33:46 +0530

    createurl done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -5,34 +5,28 @@ const urlModel = require("../models/urlModel")
 const createUrl = async (req, res) => {
     try{
 
+        const longUrl = req.body.longUrl;
         const baseUrl = 'http:localhost:3000'
-        let longUrl = req.body;
-
-    // check base url if valid using the validUrl.isUri method
-       if (!validUrl.isUri(baseUrl)) {
-         return res.status(401).send('Invalid base URL')
-      }
-
-      // if valid, we create the url code
-       const urlCode = shortid.generate()
-
-       if (validUrl.isUri(longUrl)) {
-
-        let url = await urlModel.findOne({longUrl})
-
-        // url exist and return the respose
-        if (url) {
-            res.send(url)
-        } else {
-            // join the generated short code the the base url
-            const shortUrl = baseUrl + '/' + urlCode
-            let Url = await urlModel.create(url)
-            res.status(200).send({status: true, data: Url})
-            
-       }
-    }else{
-        res.status(401).send('Invalid longUrl')
-    }
+        if(!validUrl.isUri(baseUrl)){
+            return res.status(401).json("Invalid baseUrl");
+        }
+    
+        const urlCode = shortid.generate();
+    
+        if(validUrl.isUri(longUrl)){
+    
+                let url = await urlModel.findOne({longUrl : longUrl});
+                if(url){
+                    return res.status(200).send(url);
+                }else{
+    
+                    const shortUrl = baseUrl + "/" + urlCode;
+                    url  = await urlModel.create({longUrl, shortUrl, urlCode});
+                    return res.status(201).send(url);
+                }
+        }else{
+            res.status(400).send("Invalid longUrl");
+        }    
 
     }catch(err){
         return res.status(500).send({status: false, Error: err.message})
