3ba52c47b22bbec869273472b485a959014578bd
Amit <amit261199@gmail.com>
2022-05-19 17:25:57 +0530

    done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,12 +1,8 @@
 const validUrl = require('valid-url')
 const shortid = require('shortid')
 const urlModel = require("../models/urlModel")
-
-
 const redis = require("redis");
-
 const { promisify } = require("util");
-
 
 //Connect to redis
 const redisClient = redis.createClient(
@@ -38,16 +34,16 @@ const redisClient = redis.createClient(
         const data = req.body;
         const baseUrl = 'http:localhost:3000'
 
+        
+        if(Object.keys(data).length == 0) return res.status(400).send({status: false, message: "Invalid URL Please Enter valid details"}) 
         if(!data.longUrl) return res.status(400).send({status: false, message: "longUrl is required"})
 
-        if(!validUrl.isUri(baseUrl)){
-            return res.status(401).send({status: false, message: "Invalid baseUrl"});
-        }
-    
         if(validUrl.isUri(data.longUrl)){
     
                 let getUrl = await GET_ASYNC(`${data.longUrl}`)
+                //console.log("1-->", getUrl)
                 let url = JSON.parse(getUrl)
+                //console.log("2-->", url)
                 if(url){
                     return res.status(200).send({status: true, message: "Success",data: url});
                 }else{
@@ -61,12 +57,13 @@ const redisClient = redis.createClient(
 
                     await urlModel.create(data)
                     let responseData  = await urlModel.findOne({urlCode:urlCode}).select({_id:0, __v:0});
+                    //console.log("3---->",responseData)
                     await SET_ASYNC(`${data.longUrl}`, JSON.stringify(responseData))
                     return res.status(201).send({status: true, message: "URL create successfully",data:responseData});
 
                 }
         }else{
-           return res.status(400).send({status: false, message: "Invalid longUrl"});
+           return res.status(400).send({status: false, message: "Enter a valid Url"});
         }    
 
     }catch(err){
@@ -85,7 +82,7 @@ const getUrl = async (req, res) => {
          return res.status(307).redirect(url.longUrl)
     }else{
         let code = await urlModel.findOne({urlCode: req.params.urlCode}) 
-        if(!code) return res.status(404).send({status: false, message:"No URL Found"})
+        if(!code) return res.status(404).send({status: false, message:"Url-code not Found"})
 
         await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(code))
         return res.status(307).redirect(code.longUrl);
@@ -101,53 +98,3 @@ module.exports = {createUrl, getUrl}
 
 // There is a NPM package called ‘shortid’ used to create short non-sequential url-friendly unique ids.
 //  By default, it uses 7-14 url-friendly characters: A-Z, a-z, 0-9, _-.
-
-//const createUrl = async (req, res) => {
-    //     try{
-    
-    //         const longUrl = req.body.longUrl;
-    //         const baseUrl = 'http:localhost:3000'
-    
-    //         if(!longUrl) return res.status(400).send({status: false, message: "longUrl is required"})
-    
-    //         if(!validUrl.isUri(baseUrl)){
-    //             return res.status(401).send({status: false, message: "Invalid baseUrl"});
-    //         }
-        
-    //         let urlCode = shortid.generate();
-    //         if(!urlCode) return res.status(400).send({status: false, message: "we can't find any shortid"})
-        
-    //         if(validUrl.isUri(longUrl)){
-        
-    //                 let url = await urlModel.findOne({longUrl : longUrl}).select({_id: 0, __v: 0});
-    //                 // if url exist and return the respose
-    //                 if(url){
-    //                     return res.status(200).send({status: true, message: "This longurl already created",data:url});
-    //                 }else{
-        
-    //                     // join the generated urlcode to the baseurl
-    //                     let shortUrl = baseUrl + "/" + urlCode;
-    //                     url  = await urlModel.create({longUrl, shortUrl, urlCode});
-    //                     return res.status(201).send({status: true, data:url});
-    
-    //                 }
-    //         }else{
-    //            return res.status(400).send({status: false, message: "Invalid longUrl"});
-    //         }    
-    
-    //     }catch(err){
-    //         return res.status(500).send({status: false, Error: err.message})
-    //     }
-    // }
-
-     // try{
-    //     let code = req.params.urlCode
-    //     const url = await urlModel.findOne({urlCode: code})
-    //     if (url) {
-    //         return res.status(302).redirect(url.longUrl)
-    //     } else {
-    //         return res.status(400).send('No URL Found')
-    //     }
-    // }catch(err){
-    //     return res.status(500).send({status: false, Error: err.message})
-    // }
